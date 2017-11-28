bb788b25b60f3f3b12930495eef6796497d1e2ee
Feng <chifeng1113@gmail.com>
2017-11-27 22:14:39 -0500

    modified several RBM params

--- plottests.py
+++ plottests.py
@@ -8,9 +8,13 @@ if __name__ == '__main__':
     with open('exp_data/mountainCarNoTransfer.pkl', 'rb') as f:
         x, rewards, steps = pickle.load(f)
 
-    with open('exp_data/mountainCarTransferOriginal.pkl', 'rb') as f:
+    with open('exp_data/mountainCarTransfer.pkl', 'rb') as f:
         x_t, rewards_t, steps_t = pickle.load(f)
 
+
+    plt.plot(rewards, 'b')
+    plt.plot(rewards_t, 'r')
+    plt.show()
     plot_with_gp(x, rewards, title='reward per episode', xlabel='episode', ylabel='reward', plt_lable='no transfer',
                  color='r')
     plot_with_gp(x_t, rewards_t, title='reward per episode', xlabel='episode', ylabel='reward',
